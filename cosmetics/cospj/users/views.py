0e5703a67623145281e67a6331adaffc03e13246
daniellee1011 <kauvoi1011@gmail.com>
2023-03-26 22:40:25 -0400

    [App] Create sign up function
    : Sign up function is created.
    Currently, it's using internal DB instead of MySQL.
    
    https://www.youtube.com/watch?v=sZ7p7AyijaI
    
    http://127.0.0.1:8000/admin
    http://127.0.0.1:8000/auth/login
    
    username: daniel
    password: 0000

--- cosmetics/cospj/users/views.py
+++ cosmetics/cospj/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
+from .models import User
 
 # Create your views here.
 
@@ -21,3 +22,23 @@ def logout_view(request):
     logout(request)
     
     return redirect("user:login")
+
+def signup_view(request):
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        email = request.POST["email"]
+        user_id = request.POST["user_id"]
+        
+        user = User.objects.create_user(username, email, password)
+        user.lastname = lastname
+        user.firstname = firstname
+        user.user_id = user_id
+        user.save()
+        # login(request, user) : redirect to the status already login
+        return redirect("user:login")
+        
+    return render(request, "users/signup.html")
