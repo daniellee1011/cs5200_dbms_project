aa97966d1291fdbc27df80ca77d707662ed497ba
daniellee1011 <kauvoi1011@gmail.com>
2023-04-01 23:39:33 -0400

    [Django] Create nav bar
    : Create nav bar and add search fundtion

--- cosmetics/cospj/users/urls.py
+++ cosmetics/cospj/users/urls.py
@@ -6,5 +6,6 @@ app_name = "user"
 urlpatterns = [
     path("login", views.login_view, name = "login"),
     path("logout", views.logout_view, name = "logout"),
-    path("signup", views.signup_view, name = "signup")
+    path("signup", views.signup_view, name = "signup"),
+    path("search_venues", views.search_venues, name = "search_venues")
 ]
