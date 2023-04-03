1e3965e8c6b91fe007b396f3d39b2cddf7e39e8f
yerin-Lim <yesyerin@gmail.com>
2023-04-03 18:05:43 -0400

    add product button

--- cosmetics/cospj/cospj/settings.py
+++ cosmetics/cospj/cospj/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'cosmetics',
         'USER': 'root',
-        'PASSWORD': 'Yr0206!!',
+        'PASSWORD': '0000',
         'HOST': 'localhost',   
         'PORT': '3306',
     }
